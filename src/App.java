85b15ead6eb7f5724752554def18d94da5985dea
is13mael <ismaelparramoussaif@gmail.com>
2025-10-22 19:06:11 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.print("Introduce un número y le mostrare su tabla de multiplicar: ");
+        int numero = Integer.parseInt(System.console().readLine());
+
+        int i = 1;
+        while (i <= 10){
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+            i++;
+        }
+    }
+}
